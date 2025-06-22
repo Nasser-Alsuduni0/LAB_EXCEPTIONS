f55b88f74076cc49efc367aaeef5442adf6bd6f2
Nasser-Alsuduni0 <Nasser.alsuduni@outlook.sa>
2025-06-23 02:51:09 +0300

    lap solution

---
+++ LAB_EXCEPTIONS.py
@@ -0,0 +1,12 @@
+def additoin(x, y):
+ try:
+    x = 10
+    y = 20
+    print("Addition:", x + b)
+ except NameError:
+   print("Error the variable is not defined")
+ else:
+    print("the operation is successful")
+   
+
+additoin(10, 20)
